c18de073e024e5919b5b0ccd828c88eb72adbb92
Ha1ion <143371193+Ha1ion@users.noreply.github.com>
2024-10-17 20:47:51 +0800

    Add files via upload

---
+++ frontend-new/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
